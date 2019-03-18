74c72c6d24e446dcdbd71f85757d316399ffba1a
André Santanchè <santanche@gmail.com>
2019-03-18 01:18:52 -0300

    Multilevel knots and context for variables.

---
+++ notebook/cases/case001-development/html/knots/Cycle_1.Check_hypothesis.js
@@ -0,0 +1,304 @@
+(function() { PlayerManager.instance().presentKnot(`<svg
+   xmlns:dc="http://purl.org/dc/elements/1.1/"
+   xmlns:cc="http://creativecommons.org/ns#"
+   xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#"
+   xmlns:svg="http://www.w3.org/2000/svg"
+   xmlns="http://www.w3.org/2000/svg"
+   xmlns:xlink="http://www.w3.org/1999/xlink"
+   xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd"
+   xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape"
+   width="100%"
+   height="100%"
+   viewBox="0 0 1380.8701 700"
+   version="1.1"
+   id="svg102"
+   inkscape:version="0.92.3 (2405546, 2018-03-11)"
+   sodipodi:docname="jacinto-talk-l.svg">
+  <metadata
+     id="metadata106">
+    <rdf:RDF>
+      <cc:Work
+         rdf:about="">
+        <dc:format>image/svg+xml</dc:format>
+        <dc:type
+           rdf:resource="http://purl.org/dc/dcmitype/StillImage" />
+        <dc:title></dc:title>
+      </cc:Work>
+    </rdf:RDF>
+  </metadata>
+  <sodipodi:namedview
+     pagecolor="#ffffff"
+     bordercolor="#666666"
+     borderopacity="1"
+     objecttolerance="10"
+     gridtolerance="10"
+     guidetolerance="10"
+     inkscape:pageopacity="0"
+     inkscape:pageshadow="2"
+     inkscape:window-width="731"
+     inkscape:window-height="704"
+     id="namedview104"
+     showgrid="false"
+     inkscape:zoom="0.36562501"
+     inkscape:cx="727.15592"
+     inkscape:cy="176.57909"
+     inkscape:window-x="67"
+     inkscape:window-y="27"
+     inkscape:window-maximized="0"
+     inkscape:current-layer="svg102" />
+  <defs
+     id="defs14">
+    <style
+       id="style2">
+      .cls-1, .cls-12, .cls-15, .cls-18 {
+        fill: #fff;
+      }
+
+      .cls-1, .cls-5 {
+        stroke: #a2a2a2;
+      }
+
+      .cls-2 {
+        clip-path: url(#clip-Experimente_1_5);
+      }
+
+      .cls-3 {
+        fill: url(#pattern);
+      }
+
+      .cls-4 {
+        fill: #e3e3e3;
+      }
+
+      .cls-24, .cls-5 {
+        fill: none;
+      }
+
+      .cls-6 {
+        fill: #707070;
+      }
+
+      .cls-15, .cls-18, .cls-6 {
+        font-size: 20px;
+      }
+
+      .cls-15, .cls-20, .cls-6 {
+        font-family: Tahoma, Geneva, sans-serif;
+      }
+
+      .cls-7 {
+        clip-path: url(#clip-path);
+      }
+
+      .cls-8 {
+        fill: #009b3a;
+      }
+
+      .cls-9 {
+        fill: #fedf00;
+      }
+
+      .cls-10 {
+        fill: #002776;
+      }
+
+      .cls-11 {
+        clip-path: url(#clip-path-2);
+      }
+
+      .cls-13 {
+        fill: #346e93;
+      }
+
+      .cls-14 {
+        fill: #ba2027;
+      }
+
+      .cls-16 {
+        fill: #0d4a71;
+      }
+
+      .cls-17 {
+        fill: #d6d6d6;
+      }
+
+      .cls-18, .cls-21 {
+        font-family: Tahoma, Geneva, sans-serif;
+        font-weight: 700;
+      }
+
+      .cls-19 {
+        fill: #0d7114;
+      }
+
+      .cls-20, .cls-21 {
+        fill: #545454;
+        font-size: 12px;
+      }
+
+      .cls-22 {
+        fill: #fee611;
+      }
+
+      .cls-23 {
+        stroke: none;
+      }
+    </style>
+    <pattern
+       id="pattern"
+       preserveAspectRatio="xMidYMid slice"
+       width="100%"
+       height="100%"
+       viewBox="0 0 1920 1080" />
+    <clipPath
+       id="clip-path">
+      <rect
+         id="Rectangle_32"
+         data-name="Rectangle 32"
+         class="cls-1"
+         width="48"
+         height="32"
+         rx="16"
+         transform="translate(1625,10.217)"
+         x="0"
+         y="0"
+         style="fill:#ffffff;stroke:#a2a2a2" />
+    </clipPath>
+    <clipPath
+       id="clip-path-2">
+      <circle
+         id="Ellipse_8"
+         data-name="Ellipse 8"
+         cx="8.3999996"
+         cy="8.3999996"
+         r="8.3999996" />
+    </clipPath>
+    <clipPath
+       id="clip-Experimente_1_5">
+      <rect
+         width="1920"
+         height="1080"
+         id="rect11"
+         x="0"
+         y="0" />
+    </clipPath>
+  </defs>
+  <image
+     xlink:href="images/hospital-background.png"
+     y="71.079224"
+     x="20.12265"
+     id="image4087"
+     preserveAspectRatio="none"
+     height="597.91913"
+     width="1136.7474" />
+  <g
+     data-name="Back arrow"
+     id="back-arrow"
+     transform="translate(-151,-194)">
+    <circle
+       style="fill:#346e93"
+       transform="translate(151,194)"
+       r="36.5"
+       cy="36.5"
+       cx="36.5"
+       class="cls-13"
+       data-name="Ellipse 5"
+       id="Ellipse_5" />
+    <path
+       style="fill:#ffffff"
+       inkscape:connector-curvature="0"
+       transform="translate(-653.108,45.012)"
+       d="m 863.02,182.135 h -30.292 a 0.519,0.519 0 0 1 -0.367,-0.887 l 6.732,-6.732 a 0.519,0.519 0 0 0 0,-0.734 l -4.641,-4.641 a 0.519,0.519 0 0 0 -0.734,0 l -12.995,12.995 h -0.054 v 0.054 l -3.409,3.41 a 0.52,0.52 0 0 0 0,0.735 l 16.457,16.457 a 0.519,0.519 0 0 0 0.734,0 l 4.641,-4.641 a 0.519,0.519 0 0 0 0,-0.734 l -6.788,-6.788 a 0.519,0.519 0 0 1 0.367,-0.887 h 30.349 a 0.519,0.519 0 0 0 0.519,-0.519 v -6.564 a 0.519,0.519 0 0 0 -0.519,-0.524 z"
+       class="cls-12"
+       data-name="Path 320"
+       id="Path_320" />
+  </g>
+  <foreignObject>
+     <dcc-trigger action="navigate/knot/previous" label="Previous Knot" xstyle="out-image" location="back-arrow"></dcc-trigger>
+  </foreignObject>
+  <g
+     style="fill:#ffffff;stroke:#707070"
+     class="cls-4"
+     data-name="Statement"
+     id="Statement"
+     transform="translate(50,105)">
+    <rect
+       style="stroke:none"
+       y="0"
+       x="0"
+       id="rect18"
+       rx="12"
+       height="108"
+       width="800"
+       class="cls-25" />
+    <rect
+       style="fill:none"
+       id="rect20"
+       rx="11.5"
+       height="107"
+       width="800"
+       y="0.5"
+       x="0.5"
+       class="cls-26" />
+  </g>
+  <foreignObject
+     data-name="Talk Speech"
+     x="70"
+     y="127.1966"
+     width="750"
+     height="80">
+    <div
+       id="talk-speech"
+       class="cls-15"
+       style="font-size:20px;font-family:Tahoma, Geneva, sans-serif;color:#346e93" height="420px">
+       Speech
+    </div>
+  </foreignObject>
+  <foreignObject
+     data-name="Selector"
+     x="70"
+     y="230"
+     width="800"
+     height="420">
+    <div
+       id="selector"
+       class="cls-15"
+       style="font-size:20px;font-family:Tahoma, Geneva, sans-serif;color:#ffffff" height="420px">
+       <dcc-styler xstyle="out" locations="action-1">
+          <p><dcc-talk id='dcc3' character='Jacinto' speech='Let us check out your hypothesis. Highlight in green the findings that corroborate your hypothesis; in blue those that are neutral; and in red the ones speaking against your hypothesis.'>
+</dcc-talk></p>
+<p><dcc-group-selector id='dcc5' context='Cycle_1.Check_hypothesis.symptoms1'  evaluation='contribution to diagnostics' states=' ,+,=,-'></p>
+<p>Nurse: Doctor, please you have to evaluate a man (<dcc-state-selector id='dcc7'>51 years-old</dcc-state-selector>) who entered the emergency department reporting <dcc-state-selector id='dcc9'>chest pain</dcc-state-selector>.His vital signs are <dcc-state-selector id='dcc11'>ABP: 144x92mmHG</dcc-state-selector>; <dcc-state-selector id='dcc13'>HR: 78bpm</dcc-state-selector>; <dcc-state-selector id='dcc15'>RR: 21rpm</dcc-state-selector>; <dcc-state-selector id='dcc17'>Temp: 37oC</dcc-state-selector>; <dcc-state-selector id='dcc19'>O2Sat: 98%</dcc-state-selector>.</p>
+<p>Patient: Doctor, I am feeling chest pain since yesterday. The <dcc-state-selector id='dcc21'>pain is continuous</dcc-state-selector> and <dcc-state-selector id='dcc23'>is located just in the middle of my chest</dcc-state-selector>, <dcc-state-selector id='dcc25'>worsening when I breathe</dcc-state-selector> and <dcc-state-selector id='dcc27'>when I lay down on my bed</dcc-state-selector>. I have <dcc-state-selector id='dcc29'>arterial hypertension</dcc-state-selector> and <dcc-state-selector id='dcc31'>I smoke 20 cigarettes</dcc-state-selector> every day. <dcc-state-selector id='dcc33'>My father had a "heart attack"</dcc-state-selector> at my age and I am very worried about it.</p>
+<p>You perform physical examination: <dcc-state-selector id='dcc35'>cardiac and pulmonary auscultation are normal</dcc-state-selector>; <dcc-state-selector id='dcc37'>chest pain does not worse with palpation of the thorax</dcc-state-selector>; <dcc-state-selector id='dcc39'>there is no jugular stasis</dcc-state-selector> <dcc-state-selector id='dcc41'>nor lower limb edema</dcc-state-selector>.</p>
+</dcc-group-selector>
+<p><dcc-trigger id='dcc45' type='**' link='Cycle_1.Review_hypothesis' label='Submit' ></dcc-trigger></p>
+       </dcc-styler>
+     </div>
+  </foreignObject>
+  <rect
+     style="font-size:20px;font-family:Tahoma, Geneva, sans-serif;fill:#ba2027"
+     y="641"
+     x="396"
+     rx="29.5"
+     height="59"
+     width="301"
+     class="cls-15"
+     
+     data-name="Action 1 Wrapper"
+     id="action-1-wrapper" />
+  <foreignObject
+     data-name="Action 1"
+     x="402"
+     y="658"
+     width="285"
+     height="30">
+    <div
+       id="action-1"
+       class="cls-16"
+       style="font-size:18px;font-family:Tahoma, Geneva, sans-serif;color:#ffffff;text-align:center;width:100%">
+        action
+     </div>
+  </foreignObject>
+
+</svg>`) })();
