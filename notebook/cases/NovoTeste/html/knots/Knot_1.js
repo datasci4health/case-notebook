70a6a340fc8f5f89008d59987b3ed6cf662b6135
santanche <santanche@MUSEU.PC>
2019-04-03 16:13:43 -0300

    Authoring features.

---
+++ notebook/cases/NovoTeste/html/knots/Knot_1.js
@@ -0,0 +1,5 @@
+(function() { PlayerManager.instance().presentKnot(`<div class="template-knot">
+<p>Tela 1.</p>
+<p><img src="images/doctor.png" alt="Doctor" /></p>
+<p><dcc-trigger id='dcc3' type='*' link='Knot_2' label='Knot 2' ></dcc-trigger></p>
+</div>`) })();
---
+++ notebook/cases/NovoTeste/html/knots/Knot_3.js
@@ -0,0 +1,3 @@
+(function() { PlayerManager.instance().presentKnot(`<div class="template-knot">
+<p>Tela 3.</p>
+</div>`) })();
