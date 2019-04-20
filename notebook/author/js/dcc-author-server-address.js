4b55671d330c7f9b4de0a9d3a5290ace588f68ef
Matheus Mota <smota.matheus@gmail.com>
2019-04-19 23:53:39 -0300

    Update dcc-author-server-address.js

--- notebook/author/js/dcc-author-server-address.js
+++ notebook/author/js/dcc-author-server-address.js
@@ -1,4 +1,4 @@
 (function() {
-   DCCAuthorServer.serverAddress = "http://35.247.224.186/case-notebook/latest/server/";
+   DCCAuthorServer.serverAddress = "http://35.247.224.186/case-notebook/v1/server/";
    // DCCAuthorServer.serverAddress = "http://127.0.0.1:8888/";
 })();
